2f9fce2ad608d892caf44c8be6b204bb77687b6e
Sergio Morillas <sergiomc11756>
2023-11-10 13:27:32 +0100

    Apartado de las listas separado en metodos

--- app/src/main/java/com/lista/listacompra/Listas.java
+++ app/src/main/java/com/lista/listacompra/Listas.java
@@ -1,6 +1,5 @@
 package com.lista.listacompra;
 
-import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -9,7 +8,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
