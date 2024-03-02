da6598d92398d9088e1b64a1c90a4b1679961ed6
Sergio Morillas <sergiomc11756>
2024-03-02 12:39:10 +0100

    Muestra mapas en la clase Mercadona

--- app/src/main/java/com/lista/listacompra/supermercado/Mercadona.java
+++ app/src/main/java/com/lista/listacompra/supermercado/Mercadona.java
@@ -1,6 +1,8 @@
 package com.lista.listacompra.supermercado;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,9 +14,11 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Mercadona implements Supermercado {
-    private static final String MERCADONA_API_URL = "https://7uzjkl1dj0-dsn.algolia.net/1/indexes/" +
-            "products_prod_4315_es/query?x-algolia-application-id=7UZJKL1DJ0&x-algolia-api-key=" +
-            "9d8f2e39e90df472b4f2e559a116fe17";
+    private static final String MERCADONA_API_URL = "https://7uzjkl1dj0-dsn.algolia.net/1/indexes/" + "products_prod_4315_es/query?x-algolia-application-id=7UZJKL1DJ0&x-algolia-api-key=" + "9d8f2e39e90df472b4f2e559a116fe17";
+
+    public Mercadona() {
+    }
+
     @Override
     public Map<Double, String[]> busqueda(String producto) {
         try {
@@ -22,7 +26,9 @@ public class Mercadona implements Supermercado {
 
             Request request = new Request.Builder()
                     .url(MERCADONA_API_URL)
-                    .post(RequestBody.create("{\"params\":\"query=" + producto + "&clickAnalytics=true&analyticsTags=%5B%22web%22%5D&getRankingInfo=true&hitsPerPage=10\"}", MediaType.parse("application/json")))
+                    .post(RequestBody.create(
+                    "{\"params\":\"query=" + producto + "&hitsPerPage=10\"}",
+                            MediaType.parse("application/json")))
                     .build();
 
             Response response = client.newCall(request).execute();
@@ -37,21 +43,21 @@ public class Mercadona implements Supermercado {
         Map<Double, String[]> productMap = new HashMap<>();
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(json);
+            ObjectMapper mapeador = new ObjectMapper();
+            JsonNode raiz = mapeador.readTree(json);
 
-            JsonNode hitsNode = rootNode.get("hits");
-            Iterator<JsonNode> hitsIterator = hitsNode.elements();
+            JsonNode hits = raiz.get("hits");
+            Iterator<JsonNode> hitsIterator = hits.elements();
 
             while (hitsIterator.hasNext()) {
                 JsonNode hitNode = hitsIterator.next();
 
-                Double price = hitNode.path("price_instructions").path("unit_price").asDouble();
-                String displayName = hitNode.path("display_name").asText();
-                String thumbnail = hitNode.path("thumbnail").asText();
+                Double precio = hitNode.path("price_instructions").path("unit_price").asDouble();
+                String nombre = hitNode.path("display_name").asText();
+                String descripcion = hitNode.path("thumbnail").asText();
 
-                String[] productInfo = {displayName, thumbnail};
-                productMap.put(price, productInfo);
+                String[] productInfo = {nombre, descripcion};
+                productMap.put(precio, productInfo);
             }
         } catch (Exception e) {
             System.err.println("Error en la conversion del JSON a mapa: " + e.getMessage());
@@ -59,4 +65,28 @@ public class Mercadona implements Supermercado {
 
         return productMap;
     }
+    //TODO: Cambiar la clave del producto del precio a la URL
+    public static void main(String[] args) {
+        Mercadona mer = new Mercadona();
+        Map m1 = mer.busqueda("pan");
+        Alcampo alc = new Alcampo();
+        Map m2 =alc.busqueda("pan");
+        imprimirMapa(m1);
+        imprimirMapa(m2);
+    }
+    public static void imprimirMapa(Map<Double, String[]> mapa) {
+        for (Map.Entry<Double, String[]> entry : mapa.entrySet()) {
+            Double clave = entry.getKey();
+            String[] valores = entry.getValue();
+
+            System.out.println("Clave: " + clave);
+
+            System.out.print("Valores: ");
+            for (String valor : valores) {
+                System.out.print(valor + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
